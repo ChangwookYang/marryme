dadde8fcbc491ecce944364b7a510c848d790522
changwook yang <ycw92325@gmail.com>
2023-02-08 00:45:12 +0900

    #3 - 스프링 프로젝트 생성

---
+++ src/test/java/com/marryme/MarryMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marryme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarryMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
